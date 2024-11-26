7e97fde83cdd06656fa30c01d72ec12dccae0a7f
Bi54rq <159131675+Bi54rq@users.noreply.github.com>
2024-11-26 10:20:27 +0000

    first level memory game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,165 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite bgImage;
+
+    public Sprite[] puzzles;
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
+    public List<Button> buttons = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+    void Awake()
+    {
+        puzzles = Resources.LoadAll<Sprite>("Sprites/Button");
+    }
+
+    void Start()
+    {
+        GetButtons();
+        AddListeners();
+        AddGamePuzzles();
+        Shuffle(gamePuzzles);
+        gameGuesses = gamePuzzles.Count / 2;
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            buttons.Add(objects[i].GetComponent<Button>());
+            buttons[i].image.sprite = bgImage;
+        }
+    }
+
+    void AddGamePuzzles()
+    {
+        int looper = buttons.Count;
+        int index = 0;
+
+        for (int i = 0; i < looper; i++)
+        {
+            if (index == looper / 2)
+            {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+
+            index++;
+        }
+    }
+
+    void AddListeners()
+    {
+        foreach (Button button in buttons)
+        {
+            button.onClick.AddListener(() => PickAPuzzle());
+        }
+    }
+
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log("Button named " + name);
+
+        if (!firstGuess)
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(name);
+            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+            buttons[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+
+            // Disable only the first button after it's clicked
+            buttons[firstGuessIndex].interactable = false;
+        }
+        else if (!secondGuess)
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(name);
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+            buttons[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+
+            countGuesses++;
+
+            // Disable the second button after it's clicked
+            buttons[secondGuessIndex].interactable = false;
+
+            StartCoroutine(CheckIfThePuzzlesMatch());
+        }
+    }
+
+    IEnumerator CheckIfThePuzzlesMatch()
+    {
+        // Give the player a moment to see the second guess
+        yield return new WaitForSeconds(1f);
+
+        // Disable the buttons' interactivity during match checking
+        buttons[firstGuessIndex].interactable = false;
+        buttons[secondGuessIndex].interactable = false;
+
+        if (firstGuessPuzzle == secondGuessPuzzle)
+        {
+            yield return new WaitForSeconds(0.5f);
+            // If they match, hide the images
+            buttons[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            buttons[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+
+            // Check if the game is finished
+            CheckIfTheGameIsFinished();
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            // Reset the images and re-enable the buttons for another guess
+            buttons[firstGuessIndex].image.sprite = bgImage;
+            buttons[secondGuessIndex].image.sprite = bgImage;
+
+            // Reset interactivity for the next pair of guesses
+            buttons[firstGuessIndex].interactable = true;
+            buttons[secondGuessIndex].interactable = true;
+        }
+
+        // Reset the guess flags
+        firstGuess = secondGuess = false;
+    }
+
+    void CheckIfTheGameIsFinished()
+    {
+        countCorrectGuesses++;
+
+        if (countCorrectGuesses == gameGuesses)
+        {
+            Debug.Log("Completed");
+            Debug.Log("It took you " + countGuesses + " guess(es) to finish");
+        }
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+}
